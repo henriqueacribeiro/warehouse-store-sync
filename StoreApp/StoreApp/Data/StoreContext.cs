9f99472d36d6213785b83f29de737c39917cf501
Henrique Ribeiro <henriqueacribeiro@outlook.pt>
2022-08-18 23:28:37 +0100

    [STORE] [#3] Added Product model and basic controller

---
+++ StoreApp/StoreApp/Data/StoreContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+using StoreApp.Models.Product;
+
+namespace StoreApp.Data
+{
+    public class StoreContext : DbContext
+    {
+        public StoreContext(DbContextOptions<StoreContext> options) : base(options) { }
+
+        public DbSet<Product> Products { get; set; }
+    }
+}
---
+++ StoreApp/StoreApp/Models/Product/Product.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace StoreApp.Models.Product
+{
+    public class Product
+    {
+        [Key]
+        public String Code { get; set; }
+
+        [Required] 
+        public String Name { get; set; }
+
+        [Required]
+        public String Description { get; set; }
+
+        [Required]
+        public String Price { get; set; }
+
+        public Double MaximumDiscount { get; set; }
+    }
+}
